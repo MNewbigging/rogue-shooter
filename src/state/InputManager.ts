13776614b907124f8dfed05e912819248d71d0fa
MNewbigging <mrnewbigging@gmail.com>
2022-05-17 19:17:45 +0100

    input manager update to set realtime action status

--- src/state/InputManager.ts
+++ src/state/InputManager.ts
@@ -21,20 +21,39 @@ export class InputManager {
   private keyboardListener = new KeyboardListener();
   private actionMap = new Map<InputAction, boolean>();
   private realtimeActions: InputAction[] = Object.values(InputAction);
+  private keybindings = new Map<InputAction, string>();
 
   constructor(private eventListener: GameEventListener) {
     // Initialise action map
     this.realtimeActions.forEach((action) => this.actionMap.set(action, false));
+    // Set default keybindings
+    this.setDefaultKeybindings();
   }
 
   public takingAction(action: InputAction) {
     return this.actionMap.get(action) ?? false;
   }
 
+  public update() {
+    // Check if realtime actions are taking place
+    this.realtimeActions.forEach((action) => {
+      const boundKey = this.keybindings.get(action);
+      const actionKeyPressed = this.keyboardListener.isKeyPressed(boundKey);
+      this.actionMap.set(action, actionKeyPressed);
+    });
+  }
+
   public postUpdate() {
     this.mouseListener.postUpdate();
+  }
 
-    // No longer taking realtime actions
-    this.realtimeActions.forEach((action) => this.actionMap.set(action, false));
+  private setDefaultKeybindings() {
+    // TODO - provide rebind keys UI interface & logic
+
+    // WASD
+    this.keybindings.set(InputAction.MOVE_FORWARD, 'w');
+    this.keybindings.set(InputAction.MOVE_BACKWARD, 's');
+    this.keybindings.set(InputAction.STRAFE_LEFT, 'a');
+    this.keybindings.set(InputAction.STRAFE_RIGHT, 'd');
   }
 }
